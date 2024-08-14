fc21803920b9d758b76241b195a24efa3beb0290
dilshodbek200 <raxdilshod53@gmail.com>
2024-08-14 17:47:49 +0300

    project5

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Random rnd = new Random();
+        int num = rnd.Next(1,100);
+
+        for(int i = 0; i <= 9; i++)
+        {
+            Console.WriteLine("Randomni sonni topishga urinib ko'ring");
+            int son1 = Convert.ToInt32(Console.ReadLine());
+
+            if(i != 9)
+            {
+                if(son1 == num)
+                {
+                    Console.WriteLine("Siz Random sonni to'g'ri topdingiz");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Yo'q");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Siz random sonni topish uchun 10 marta urindingiz.");
+                Console.WriteLine($"Random son {num} edi.");
+            }
+        }
+    }
+}
